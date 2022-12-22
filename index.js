da67f768cd193133c01775267ed67bd393f95545
Phát Nguyễn <vanphatct2002@gmail.com>
2022-12-23 01:39:53 +0700

    update home page

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.send('home page')
+    res.send(`
+    <h1>home page</h1>
+    <a href="./about">about page</a>
+    <br>
+    <a href="./contact">contact page</a>
+    <br>
+    <br>
+    <img src="https://dean2020.edu.vn/wp-content/uploads/2021/04/anh-welcome-1.jpg" alt="">    
+    `)
 })
 
 app.get('/about', (req, res) => {
