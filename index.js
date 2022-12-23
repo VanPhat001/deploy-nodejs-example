a2ebeb32c7878cd81da3ceb1f170082e4337e4b6
Phát Nguyễn <vanphatct2002@gmail.com>
2022-12-23 09:09:13 +0700

    connect db

--- index.js
+++ index.js
@@ -7,12 +7,39 @@ const PORT = process.env.PORT || 9000
 app.use(cors())
 app.use(express.json())
 
+const { MongoClient } = require('mongodb')
+const MONGODB_URI = "mongodb+srv://admin:b8CxCsv2W6Q3feOy@cluster0.c9re2w6.mongodb.net/?retryWrites=true&w=majority";
+
+const ConnectDB = async function () {
+    return await MongoClient.connect(MONGODB_URI)
+}
+
+
+class MongoDB 
+{
+    static async connect() {
+        if (this.client) return this.client
+        this.client = await MongoClient.connect(MONGODB_URI)
+        return this.client
+    }
+}
+
+MongoDB.connect()
+    .then(() => {
+        console.log('ket noi thang cong')
+    })
+    .catch(() => {
+        console.log('ket noi that bai')
+    })
+
+
+
 app.get('/', (req, res) => {
     res.send(`
     <h1>home page</h1>
     <a href="./about">about page</a>
     <br>
-    <a href="./contact">contact page</a>
+    <a href="./info">info page</a>
     <br>
     <br>
     <img src="https://dean2020.edu.vn/wp-content/uploads/2021/04/anh-welcome-1.jpg" alt="">    
@@ -23,8 +50,11 @@ app.get('/about', (req, res) => {
     res.send('about page')
 })
 
-app.get('/contact', (req, res) => {
-    res.send('contact page')
+app.get('/info',async (req, res) => {
+    const Info = MongoDB.client.db('dbExample').collection('infos')
+
+    const data = await Info.find({}).toArray()
+    res.send(data)
 })
 
 app.listen(PORT, () => {
